d436fc4c6518f425111fb330de41d8f3aa7d98f2
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-08-30 23:53:38 +0100

    binary tree leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures height of binary tree.
+ * @tree: pointer to the root of the tree.
+ *
+ * Return: 0 if @tree is NULL;
+ * otherwise, the height of @tree.
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	binary_tree_t *left, *right;
+	size_t left_leaves, right_leaves, all_leaves;
+
+	if (tree == NULL)
+		return (0);
+
+	left = tree->left;
+	right = tree->right;
+
+	if (left == NULL && right == NULL)
+		return (1);
+
+	left_leaves = binary_tree_leaves(left);
+	right_leaves = binary_tree_leaves(right);
+
+	all_leaves = left_leaves + right_leaves;
+
+	return (all_leaves);
+}
