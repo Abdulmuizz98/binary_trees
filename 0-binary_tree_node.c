da8f120b319b3dd50433b0e0d53e7d42aea510ce
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-08-30 01:59:41 +0100

    create a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,7 +17,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	new_node->parent = parent;
+	if (parent != NULL)
+	{
+		new_node->parent = parent;
+	}
 	new_node->n = value;
 
 	return (new_node);	
