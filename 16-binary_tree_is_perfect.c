a39e67afaced007508a8926dc03401c4379e7739
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-08-31 12:18:28 +0100

    perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -81,6 +81,8 @@ int power(int a, int b)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 	/*
 	 *	----------- condition for perfect tree -----------
 	 *	number of leaves of the tree == 2 ^ height of tree
